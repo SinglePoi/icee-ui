d367fc34e7e23e71a551ffa222c8a5e77bc78d49
singlepoi <rotate13@outlook.com>
2023-11-28 14:11:55 +0800

    🦄 refactor(components): version updated
    vue and vite and replace unplugin-vue-macros

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import DefineOptions from 'unplugin-vue-define-options/vite'
+// import DefineOptions from 'unplugin-vue-define-options/vite'
+import VueMacros from 'unplugin-vue-macros/vite'
+import Inspect from 'vite-plugin-inspect'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), DefineOptions()],
+  plugins: [
+    VueMacros({
+      plugins: {
+        vue: vue(),
+        // vueJsx: VueJsx(), // if needed
+      },
+    }),
+    Inspect(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
