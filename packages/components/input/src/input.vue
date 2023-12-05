27f858f31d3e9cb5f3d0b243ea3a3a352225e891
singlepoi <rotate13@outlook.com>
2023-12-05 12:39:20 +0800

    🌈 style(input): add style

--- packages/components/input/src/input.vue
+++ packages/components/input/src/input.vue
@@ -7,6 +7,7 @@
         :class="ns.e('inner')"
         :placeholder="placeholder"
         @input="handleInput"
+        @blur="handleBlur"
         @compositionstart="handleCompositionStart"
         @compositionupdate="handleCompositionUpdate"
         @compositionend="handleCompositionEnd"
@@ -16,10 +17,11 @@
 </template>
 
 <script lang="ts" setup>
-import { shallowRef, computed, onMounted, nextTick, ref } from 'vue'
+import { shallowRef, computed, onMounted, nextTick, ref, inject } from 'vue'
 import { useNamespace } from '@icee-ui/hooks'
 import { inputEmits, inputProps } from './input'
 import { UPDATE_MODEL_EVENT } from '@icee-ui/constants'
+import { formItemContextKey } from '../../form/src/constants'
 
 defineOptions({
   name: 'IcInput',
@@ -27,25 +29,21 @@ defineOptions({
 
 const props = defineProps(inputProps)
 const emit = defineEmits(inputEmits)
+const formItem = inject(formItemContextKey, undefined)
 
 const ns = useNamespace('input')
 
-// 组件的初始值由 props.modelValue 提供，使用 computed 是为了建立一个本地的计算缓存
 const nativeInputValue = computed(() =>
   !props.modelValue ? '' : String(props.modelValue)
 )
 
-// 设置组件的值
 const setNativeInputValue = () => {
   const input = _ref.value
-  // 在 已有值且值没有发生变化 的情况下不进行赋值
   if (!input || input.value === nativeInputValue.value) return
   input.value = nativeInputValue.value
 }
 
-// input 元素的引用
 const input = shallowRef<TargetElement>()
-// 进行缓存计算，因为后续还需要和 textarea 一起
 const _ref = computed(() => input.value)
 
 onMounted(() => {
@@ -56,14 +54,17 @@ type TargetElement = HTMLInputElement
 
 const isComposing = ref(false)
 
+const handleBlur = (event: FocusEvent) => {
+  emit('blur', event)
+  formItem?.validate?.('blur')
+}
+
 const handleInput = async (event: Event) => {
   // 如果正在使用拼音输入法，忽略该 input 事件
   if (isComposing.value) return
   const { value } = event.target as TargetElement
-  // 发射 v-model 的 update:modelValue 监听事件
   emit(UPDATE_MODEL_EVENT, value)
   await nextTick()
-  // 等待 DOM 更新后设置 input 表单的值
   setNativeInputValue()
 }
 
