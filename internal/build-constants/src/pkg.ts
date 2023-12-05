8fe4f81daeb3ae27a4eed5320b066326362bf21e
singlepoi <rotate13@outlook.com>
2023-12-05 17:30:50 +0800

    🎉 init(build):init internal

---
+++ internal/build-constants/src/pkg.ts
@@ -0,0 +1,5 @@
+export const PKG_PREFIX = '@icee-ui'
+export const PKG_NAME = 'icee-ui'
+export const PKG_CAMELCASE_NAME = 'IceeUI'
+export const PKG_CAMELCASE_LOCAL_NAME = 'IceeUILocale'
+export const PKG_BRAND_NAME = 'Icee UI'
---
+++ packages/icee-ui/component.ts
@@ -0,0 +1,7 @@
+import { IcIcon } from '@icee-ui/components/icon'
+import { IcButton, IcButtonGroup } from '@icee-ui/components/button'
+import { IcForm, IcFormItem } from '@icee-ui/components/form'
+
+import type { Plugin } from 'vue'
+
+export default [IcIcon, IcButton, IcButtonGroup, IcForm, IcFormItem] as Plugin[]
